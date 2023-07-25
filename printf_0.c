7c8dfe795206c54b636e1404f594d54b86790cc2
ISSAM-SLI <issam.slimani.maroc@gmail.com>
2023-07-25 13:46:18 -0700

    Write a function that produces output according to a format.

--- printf_0.c
+++ printf_0.c
@@ -1,10 +1,59 @@
-#include <stdio.h>
-#include "main.h"
+/**
+ * _printf - Implementation of Printf function
+ * @format: format pinter
+ * Return: the number of characters printed (excluding the null byte)
+ */
 
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...)
 {
+	int count = 0;
+	va_list args;
+	int var;
 
-    printf("%s", format);
-    return (0);
+	va_start(args, format);
+
+	for (var = 0; format[var] != '\0'; var++)
+	{
+		if (format[var] == '%')
+		{
+			var++;
+			if (format[var] == '%')
+			{
+				putchar('%');
+				count++;
+			}
+			else if (format[var] == 'c')
+			{
+				char c = va_arg(args, int);
+
+				putchar(c);
+				count++;
+			}
+			else if (format[var] == 's')
+			{
+				char *str = va_arg(args, char*);
+
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					count++;
+				}
+			}
+		}
+		else
+		{
+			putchar(format[var]);
+			count++;
+		}
+	}
+
+	va_end(args);
+
+	return (count);
 }
